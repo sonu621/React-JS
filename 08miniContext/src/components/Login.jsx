07c0d60875a8703de4eba5018625fc698ba76428
sonu621 <sanjiwgpt55@gmail.com>
2025-12-07 14:56:16 +0300

    Update the Login.jsx code

--- 08miniContext/src/components/Login.jsx
+++ 08miniContext/src/components/Login.jsx
@@ -1,24 +1,35 @@
-import React, {useState, useContext} from 'react'
-import UserContext from '../Context/UserContext'    
-import { use } from 'react'
+import React, { useState, useContext } from "react";
+import UserContext from "../Context/UserContext";
+import { use } from "react";
 
 function Login() {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const {setUser} = useContext(UserContext)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { setUser } = useContext(UserContext);
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        setUser({username, password})
-    }
-    return (
-        <div>
-            <h2>Login</h2>
-            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder='username' />
-            <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='password' />
-            <button onClick={handleSubmit}>Submit</button>
-        </div>
-    )
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUser({ username, password });
+  };
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="username"
+      />
+     {"/* // Add space between inputs */ "}
+      <input
+        type="text"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="password"
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
